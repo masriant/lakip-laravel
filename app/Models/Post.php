9d479e3ff8dbd7e4c75889f00d0b18edaf6f7855
masrianto <info.masrianto@gmail.com>
2021-09-09 12:25:12 +0700

    data collect

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -26,6 +26,20 @@ class Post
 
     public static function all()
     {
-        return self::$blog_posts;
+        return collect(self::$blog_posts);
+    }
+
+    public static function find($slug)
+    {
+        $posts = self::$blog_posts;
+            $posts = static::all();
+    //         $post = [];
+    //         foreach($posts as $p) {
+    //             if($p["slug"] === $slug) {
+    //                 $post = $p;
+    //     }
+    // }
+
+    return $posts->firstwhere('slug', $slug);
     }
 }
