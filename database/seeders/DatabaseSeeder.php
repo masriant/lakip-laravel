8fb2ca7c371faee8ab272c36d4ae3917dbb7c96f
masrianto <info.masrianto@gmail.com>
2021-09-09 21:29:34 +0700

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,79 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Masrianto',
+            'email' => 'masrianto@lakip.co.id',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Muh Zainal',
+            'email' => 'Zainalmuh@lakip.co.id',
+            'password' => bcrypt('54321')
+        ]);
+
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@lakip.co.id',
+            'password' => bcrypt('98765')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque placeat corrupti harum quis repudiandae modi dolores, deserunt ratione sed id, sit dolor voluptatibus distinctio nihil necessitatibus deleniti voluptas.',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque placeat corrupti harum quis repudiandae modi dolores, deserunt ratione sed id, sit dolor voluptatibus distinctio nihil necessitatibus deleniti voluptas. Excepturi atque fugiat sit sint beatae perferendis error ratione voluptatibus assumenda amet nam magnam consequatur molestias nostrum deleniti, nemo dolores sapiente provident!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos odio omnis molestias corrupti nesciunt qui suscipit a totam architecto quo.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates et culpa officia voluptas ad necessitatibus consectetur recusandae eveniet quos numquam, deleniti accusamus error voluptatem. Harum voluptatum ducimus impedit debitis, ut vitae aliquam ullam excepturi, nisi nemo placeat doloribus tempore! Consequuntur!</p>',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque placeat corrupti harum quis repudiandae modi dolores, deserunt ratione sed id, sit dolor voluptatibus distinctio nihil necessitatibus deleniti voluptas.',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque placeat corrupti harum quis repudiandae modi dolores, deserunt ratione sed id, sit dolor voluptatibus distinctio nihil necessitatibus deleniti voluptas. Excepturi atque fugiat sit sint beatae perferendis error ratione voluptatibus assumenda amet nam magnam consequatur molestias nostrum deleniti, nemo dolores sapiente provident!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos odio omnis molestias corrupti nesciunt qui suscipit a totam architecto quo.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates et culpa officia voluptas ad necessitatibus consectetur recusandae eveniet quos numquam, deleniti accusamus error voluptatem. Harum voluptatum ducimus impedit debitis, ut vitae aliquam ullam excepturi, nisi nemo placeat doloribus tempore! Consequuntur!</p>',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque placeat corrupti harum quis repudiandae modi dolores, deserunt ratione sed id, sit dolor voluptatibus distinctio nihil necessitatibus deleniti voluptas.',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque placeat corrupti harum quis repudiandae modi dolores, deserunt ratione sed id, sit dolor voluptatibus distinctio nihil necessitatibus deleniti voluptas. Excepturi atque fugiat sit sint beatae perferendis error ratione voluptatibus assumenda amet nam magnam consequatur molestias nostrum deleniti, nemo dolores sapiente provident!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos odio omnis molestias corrupti nesciunt qui suscipit a totam architecto quo.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates et culpa officia voluptas ad necessitatibus consectetur recusandae eveniet quos numquam, deleniti accusamus error voluptatem. Harum voluptatum ducimus impedit debitis, ut vitae aliquam ullam excepturi, nisi nemo placeat doloribus tempore! Consequuntur!</p>',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque placeat corrupti harum quis repudiandae modi dolores, deserunt ratione sed id, sit dolor voluptatibus distinctio nihil necessitatibus deleniti voluptas.',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque placeat corrupti harum quis repudiandae modi dolores, deserunt ratione sed id, sit dolor voluptatibus distinctio nihil necessitatibus deleniti voluptas. Excepturi atque fugiat sit sint beatae perferendis error ratione voluptatibus assumenda amet nam magnam consequatur molestias nostrum deleniti, nemo dolores sapiente provident!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos odio omnis molestias corrupti nesciunt qui suscipit a totam architecto quo.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates et culpa officia voluptas ad necessitatibus consectetur recusandae eveniet quos numquam, deleniti accusamus error voluptatem. Harum voluptatum ducimus impedit debitis, ut vitae aliquam ullam excepturi, nisi nemo placeat doloribus tempore! Consequuntur!</p>',
+            'category_id' => 1,
+            'user_id' => 2
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kelima',
+            'slug' => 'judul-ke-lima',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque placeat corrupti harum quis repudiandae modi dolores, deserunt ratione sed id, sit dolor voluptatibus distinctio nihil necessitatibus deleniti voluptas.',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque placeat corrupti harum quis repudiandae modi dolores, deserunt ratione sed id, sit dolor voluptatibus distinctio nihil necessitatibus deleniti voluptas. Excepturi atque fugiat sit sint beatae perferendis error ratione voluptatibus assumenda amet nam magnam consequatur molestias nostrum deleniti, nemo dolores sapiente provident!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos odio omnis molestias corrupti nesciunt qui suscipit a totam architecto quo.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates et culpa officia voluptas ad necessitatibus consectetur recusandae eveniet quos numquam, deleniti accusamus error voluptatem. Harum voluptatum ducimus impedit debitis, ut vitae aliquam ullam excepturi, nisi nemo placeat doloribus tempore! Consequuntur!</p>',
+            'category_id' => 1,
+            'user_id' => 3
+        ]);
+
     }
 }
